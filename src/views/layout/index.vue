4221bc519473cf398fb1da2608a6beb9f2a00ea1
huahua <lt1668502267@163.com>
2024-04-09 23:29:26 +0800

    feat:init

---
+++ src/views/layout/index.vue
@@ -0,0 +1,40 @@
+<script setup>
+const menuInfo = [
+    {}
+]
+</script>
+
+<template>
+  <div class="layout-wrap">
+    <div class="header">
+
+    </div>
+    <div class="main">
+        <div class="main-left">
+      <el-menu
+        default-active="2"
+        class="el-menu-vertical-demo"
+        @open="handleOpen"
+        @close="handleClose"
+      >
+      
+      </el-menu>
+        </div>
+        <div class="main-right">
+
+        </div>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.layout-wrap{
+    width: 100%;
+    height: 100%;
+}
+.main{
+    &-left{
+        width: 200px;
+    }
+}
+</style>
